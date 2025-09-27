a8cd4ac974f7b22b3e9cb13e9d00f208bb0916bf
HosniyeHaghighi <hosniyehaghighi00@gmail.com>
2025-09-27 12:54:59 +0330

    init commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { PButton } from 'pandora-design-system'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper"></div>
+  </header>
+
+  <main>
+    <p-button type="secondary">jfnvjkerkjv</p-button>
+  </main>
+</template>
+
+<style scoped></style>
