b718fb7b17765db90fea8fe2574d6aba4a1e2ae8
HosniyeHaghighi <hosniyehaghighi00@gmail.com>
2025-09-28 12:50:38 +0330

    feat(pandoraheader): add style

--- src/components/PandoraHeader.vue
+++ src/components/PandoraHeader.vue
@@ -1,20 +1,24 @@
 <template>
-  <div class="w-full flex items-center justify-between p-5">
+  <div class="w-full flex items-center justify-between p-5 bg-white">
     <div class="flex flex-col">
+      <!-- <PBadge :icon="icon"></PBadge> -->
       <PBreadcrumb>
         <span>کاربران</span>
         <span>کاربر 45675</span>
       </PBreadcrumb>
-      <span class="p-text-primary-60">جزئیات پرونده و مشخصات کاربری</span>
-      <!-- <PMenu></PMenu> -->
+      <span class="p-text-primary-60 font-weight: 500">جزئیات پرونده و مشخصات کاربری</span>
     </div>
-    <div class="">
-      <PButton>test</PButton>
+    <div>
+      <PButton type="secondary" outline :start-icon="closeCircleBoldDuotone"
+        >احراز هویت نشده</PButton
+      >
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { PButton, PBreadcrumb } from 'pandora-design-system'
+import { PButton, PBreadcrumb, PBadge } from 'pandora-design-system'
+import closeCircleBoldDuotone from 'pandora-icons/closeCircleBoldDuotone'
+
 // import userAi2Iconly from 'pandora-icons/userAi2Iconly'
 </script>
